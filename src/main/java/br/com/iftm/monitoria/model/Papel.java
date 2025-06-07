481f97566f7db8e984ace1b8594e46855bee0763
igsem123 <raphael.moreira@estudante.iftm.edu.br>
2025-06-06 21:23:47 -0300

    Adiciona estrutura inicial do projeto com configuração do Maven, correção nas classes de modelo e templates para gerenciamento de usuários.

--- src/main/java/br/com/iftm/monitoria/model/Papel.java
+++ src/main/java/br/com/iftm/monitoria/model/Papel.java
@@ -1,6 +1,7 @@
 package br.com.iftm.monitoria.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Papel {
@@ -8,7 +9,15 @@ public class Papel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "O nome do papel é obrigatório.")
     private String nome;
+
+    // Construtor
+    public Papel(String nome) {
+        this.nome = nome;
+    }
+
+    public Papel() {}
 
     public Long getId() {
         return id;
